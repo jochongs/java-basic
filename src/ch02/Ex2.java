b7936dafb286ddbf19729b06783048d547d7e24c
jochongs <joch2712@naver.com>
2024-05-22 21:03:05 +0900

    Create Ex2

---
+++ src/ch02/Ex2.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        // auto type casting
+        float f = 1234;
+
+        byte b = 127;
+
+        // 기존의 값을 최댛나 보존할 수 있는 타입으로 자동 형변환
+    }
+}
