d9cd17dc515e52da1f156eadc9cc84d355999b67
jochongs <joch2712@naver.com>
2024-05-22 20:51:39 +0900

    Create Ex4

---
+++ src/ch01/Ex4.java
@@ -0,0 +1,16 @@
+package ch01;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        int octNum = 010; // 8
+        int hexNum = 0x10; // 16
+
+        long bigNum = 1000L;
+        long hex = 0xFFFF_FFFFL;
+
+        long a = 1000;
+
+        float pi = 3.14f;
+        double rate = 1.168d;
+    }
+}
