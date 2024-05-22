22b2fcfdeb276b99881682b58a4922a7dd624a2d
jochongs <joch2712@naver.com>
2024-05-22 20:57:58 +0900

    Creat Ex6

---
+++ src/ch01/Ex6.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int age = 21;
+
+        System.out.printf("age: %d", age);
+        System.out.println();
+        System.out.printf("[%20s]", "안녕하세요");
+    }
+}
