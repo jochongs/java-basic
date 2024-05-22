f011ab243e52faa1b1687c0b71daa85077f99baa
jochongs <joch2712@naver.com>
2024-05-22 21:22:56 +0900

    Creat Ex2

---
+++ src/ch05/Ex2.java
@@ -0,0 +1,9 @@
+package ch05;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int[] iArr1 = new int[]{1, 2, 3, 4, 5};
+
+        String[] sArr1 = new String[]{"hi", "my", "name", "is"};
+    }
+}
