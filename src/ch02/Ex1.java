d7564461c368e8e19d73ebdb0fc89ed8865d57e8
jochongs <joch2712@naver.com>
2024-05-22 21:02:18 +0900

    Create Ex1

---
+++ src/ch02/Ex1.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        double d = 85.4;
+
+        int score = (int)d; // casting
+
+        System.out.println("score = " + score);
+    }
+}
