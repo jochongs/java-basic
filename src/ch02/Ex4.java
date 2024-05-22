e3f7029026cdb0562610bb950f38d59f3676e1f7
jochongs <joch2712@naver.com>
2024-05-22 21:13:19 +0900

    Create Ex4

---
+++ src/ch02/Ex4.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        float result = Math.round(40.56 * 10) / 10.0f;
+
+        System.out.println("result = " + result);
+    }
+}
