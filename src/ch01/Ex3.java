b71f24cdd767f70cbad37657a42d8030e69e1c6c
jochongs <joch2712@naver.com>
2024-05-22 20:48:33 +0900

    Create Ex3

---
+++ src/ch01/Ex3.java
@@ -0,0 +1,15 @@
+package ch01;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        final int MAX_SPEED = 10;
+
+        System.out.println("MAX_SPEED = " + MAX_SPEED);
+
+        final int TEMP_VALUE;
+
+        TEMP_VALUE = 100;
+
+//        TEMP_VALUE = 200; // ERROR
+    }
+}
