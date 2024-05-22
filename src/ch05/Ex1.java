c6f510abf034e07dff790e2e470d1cd2be3178ee
jochongs <joch2712@naver.com>
2024-05-22 21:18:45 +0900

    Create Ex1

--- src/ch05/Ex1.java
+++ src/ch05/Ex1.java
@@ -1,2 +1,10 @@
-package ch05;public class Ex1 {
+package ch05;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int[] score = {1, 2, 3, 4, 5, 6};
+        int[] tempArray = new int[5];
+
+        System.out.println("score.length = " + score.length);
+    }
 }
