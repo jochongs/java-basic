37e6cdd7d49f59acd053e2d2551d89cc3dab42ca
jochongs <joch2712@naver.com>
2024-05-22 20:53:28 +0900

    Create Ex5

---
+++ src/ch01/Ex5.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        char ch = ' ';
+//        char chError = '';
+        String str = "";
+
+        String name = new String("my name");
+        String name2 = "my name";
+
+        System.out.println(name.equals(name2));
+    }
+}
