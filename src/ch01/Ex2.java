d02036fe1f1e143b0efe70ce7c50029bf81c3045
jochongs <joch2712@naver.com>
2024-05-22 20:47:19 +0900

    Create Ex2

---
+++ src/ch01/Ex2.java
@@ -0,0 +1,15 @@
+package ch01;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int x = 100;
+        double pi = 3.14;
+        char ch = 'a';
+        String str = "abc";
+
+        System.out.println(x);
+        System.out.println(pi);
+        System.out.println(ch);
+        System.out.println(str);
+    }
+}
