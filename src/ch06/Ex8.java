619cf07d74c1b826dca896777d0b36189e4a403f
jochongs <joch2712@naver.com>
2024-05-23 23:19:17 +0900

    Create Ex8

---
+++ src/ch06/Ex8.java
@@ -0,0 +1,15 @@
+package ch06;
+
+public class Ex8 {
+    static {
+        System.out.println("static: {}");
+    }
+
+    {
+        System.out.println("instance: {}");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
