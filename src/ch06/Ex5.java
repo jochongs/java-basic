932f50e62467fdef612fd7c6f99e209d4059a373
jochongs <joch2712@naver.com>
2024-05-23 22:48:21 +0900

    Create Ex5

---
+++ src/ch06/Ex5.java
@@ -0,0 +1,12 @@
+package ch06;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        ConstructorTest constructorTest = new ConstructorTest();
+    }
+
+    public static class ConstructorTest {
+        // 생략 가능
+        ConstructorTest() {}
+    }
+}
