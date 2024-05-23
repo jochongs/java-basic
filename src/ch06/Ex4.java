c26b0cfa7a36dede6695768f9bf7d0c4b433e920
jochongs <joch2712@naver.com>
2024-05-23 22:46:43 +0900

    Create intArr add method

--- src/ch06/Ex4.java
+++ src/ch06/Ex4.java
@@ -6,6 +6,8 @@ public class Ex4 {
         int int2 = 2;
         double dou1 = 1;
         double dou2 = 2;
+        int[] intArr = new int[]{1, 2, 3, 4, 5};
+
 
         int result1 = add(int1, int2);
         System.out.println("result1 = " + result1);
@@ -15,6 +17,9 @@ public class Ex4 {
 
         double result3 = add(dou1, dou2);
         System.out.println("result3 = " + result3);
+
+        int result4 = add(intArr);
+        System.out.println("result4 = " + result4);
     }
 
     public static int add(int num1, int num2) {
@@ -27,5 +32,15 @@ public class Ex4 {
 
     public static  double add(double num1, double num2) {
         return num1 + num2;
+    }
+
+    public static int add(int[] intArr) {
+        int sum = 0;
+
+        for(int i = 0; i < intArr.length; i++) {
+            sum += intArr[i];
+        }
+
+        return sum;
     }
 }
