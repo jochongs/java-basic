5309dd3952b5f59b97cedfe75a1417f3fa23fdbb
jochongs <joch2712@naver.com>
2024-05-23 22:59:58 +0900

    Add empty property

--- src/ch06/Ex6.java
+++ src/ch06/Ex6.java
@@ -3,11 +3,18 @@ package ch06;
 public class Ex6 {
     public static void main(String[] args) {
         Person personDefaultConstructor = new Person();
+
+        Person personNotDefaultConstructor = new Person("jochongs", 23);
+
+        System.out.println("personDefaultConstructor = " + personDefaultConstructor);
+        System.out.println("personNotDefaultConstructor = " + personNotDefaultConstructor);
     }
 
     public static class Person {
         public String name;
         public int age;
+        char a;
+        boolean testBool;
 
         Person() {}
 
@@ -18,7 +25,7 @@ public class Ex6 {
 
         @Override
         public  String toString() {
-            return "name: " + name + "   age: " + age;
+            return "name: " + name + "   age: " + age + "      bool:" + testBool + "      char: " + a;
         }
     }
 }
