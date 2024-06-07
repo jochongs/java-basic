c8bad2011e9725c75aecf5fbebab59378f52571d
jochongs <joch2712@naver.com>
2024-06-07 16:36:20 +0900

    Create Ex2

---
+++ src/ch08/Ex2.java
@@ -0,0 +1,15 @@
+package ch08;
+
+public class Ex2 {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    static void method1() throws Exception {
+        method2();
+    }
+
+    static void method2() throws Exception {
+        throw new Exception();
+    }
+}
