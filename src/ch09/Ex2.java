1a624d201b61cb173642a09c7d9ecaf7a51045ff
jochongs <joch2712@naver.com>
2024-06-07 16:42:33 +0900

    Create Ex2

---
+++ src/ch09/Ex2.java
@@ -0,0 +1,10 @@
+package ch09;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int num1 = 100;
+        String stringNum = String.valueOf(num1);
+
+        System.out.println(stringNum);
+    }
+}
